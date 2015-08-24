2663d819bf6d3d8c1e785445463ebe4835cf6a17
Bartosz Jaroszewski <b.jarosze@gmail.com>
2015-08-24 19:59:43 +0000

    added helper for html tag components

--- lib/riot_js/rails/helper.rb
+++ lib/riot_js/rails/helper.rb
@@ -2,18 +2,48 @@ module RiotJs
   module Rails
     module Helper
 
-      def riot_component(name, options, &block)
-        component_name = name.to_s.gsub('_', '-')
-        attributes = { id: "riot-#{component_name}-#{Random.rand(10000000)}",
-                       class: 'riot-rails-component',
-                       data: { opts: options.to_json } }
-
-        if block_given?
-          content_tag(component_name, attributes, &block)
-        else
-          content_tag(component_name, attributes) {}
+      def riot_component(*args, &block)
+        case args.count
+          when 2
+            custom_tag_riot_component(*args, &block)
+          when 3
+            html_tag_riot_component(*args, &block)
+          else
+            raise ArgumentError, 'wrong number of arguments (required 2 or 3)'
         end
+      end
 
+      private
+
+      def custom_tag_riot_component(name, data, &block)
+        component_name, attributes = component_attributes(name, data)
+
+        component_tag(component_name, attributes, &block)
+      end
+
+      def html_tag_riot_component(tag, name, data, &block)
+        component_name, attributes = component_attributes(name, data)
+        attributes['riot-tag'] = component_name
+
+        component_tag(tag, attributes, &block)
+      end
+
+      def component_attributes(name, data)
+        component_name = name.to_s.gsub('_', '-')
+        attributes = {
+          id: "riot-#{component_name}-#{Random.rand(10000000)}",
+          class: 'riot-rails-component',
+          data: { opts: data.to_json }
+        }
+        return component_name, attributes
+      end
+
+      def component_tag(tag_name, attributes, &block)
+        if block_given?
+          content_tag(tag_name, attributes, &block)
+        else
+          content_tag(tag_name, attributes) {}
+        end
       end
 
     end

8cf99facd3b746a0082399dedb5d184314df229a
Osamu Takeuchi <osamu@big.jp>
2016-10-12 22:30:10 +0900

    add support for sprockets version 2, 3 and 4

--- lib/riot_js/rails/railtie.rb
+++ lib/riot_js/rails/railtie.rb
@@ -9,11 +9,35 @@ module RiotJs
       config.riot.node_paths = []
 
       initializer :setup_sprockets do |app|
-        Processor.register_self config
+        # app is a YourApp::Application
+        # config is Rails::Railtie::Configuration that belongs to RiotJs::Rails::Railtie
+        Processor.register_self app, config
 
         if defined?(::Haml)
           require 'tilt/haml'
-          app.assets.register_engine '.haml', ::Tilt::HamlTemplate
+          Haml::Template.options[:format] = :html5
+
+          if config.respond_to?(:assets)
+            config.assets.configure do |env|
+              if env.respond_to?(:register_transformer)
+                # Sprockets 3 and 4
+                env.register_mime_type 'text/riot-tag+haml', extensions: ['.tag.haml'], charset: :html
+                env.register_transformer 'text/riot-tag+haml', 'application/javascript',
+                        Proc.new{ |input| Processor.call(::Haml::Engine.new(input[:data]).render) }
+              elsif env.respond_to?(:register_engine)
+                if Sprockets::VERSION.start_with?("3")
+                  # Sprockets 3 ... is this needed?
+                  env.register_engine '.haml', ::Tilt::HamlTemplate, { silence_deprecation: true }
+                else
+                  # Sprockets 2.12.4
+                  env.register_engine '.haml', ::Tilt::HamlTemplate
+                end
+              end
+            end
+          else
+            # Sprockets 2
+            app.assets.register_engine '.haml', ::Tilt::HamlTemplate
+          end
         end
       end
 
@@ -31,7 +55,6 @@ module RiotJs
 
         ENV['NODE_PATH'] = node_paths.join(':')
       end
-
 
       def detect_node_global_path
         prefix = `npm config get prefix`.to_s.chomp("\n")

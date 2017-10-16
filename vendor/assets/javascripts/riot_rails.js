139f30228dc7d3cb0a5352fdde640833cd813063
Pascal Huber <pascal.huber@resolved.ch>
2017-10-16 19:27:08 +0200

    added turbolinks 5 support

--- vendor/assets/javascripts/riot_rails.js
+++ vendor/assets/javascripts/riot_rails.js
@@ -38,25 +38,35 @@
 
   var handleTurbolinksPageLoad = function () {
     var unmountEvent;
+    var mountEvent;
 
-    if (Turbolinks.EVENTS) {
-      unmountEvent = Turbolinks.EVENTS.BEFORE_UNLOAD;
-    } else {
-      unmountEvent = 'page:receive';
-      Turbolinks.pagesCached(0);
-
-      if (window.ReactRailsUJS.RAILS_ENV_DEVELOPMENT) {
-        console.warn('The Turbolinks cache has been disabled (Turbolinks >= 2.4.0 is recommended).');
+    if (typeof Turbolinks.EVENTS !== 'undefined') {
+      if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
+        // Turbolinks.EVENTS is in classic version 2.4.0+
+        mountEvent = 'page:change';
+        unmountEvent = Turbolinks.EVENTS.BEFORE_UNLOAD;
+      } else if (typeof Turbolinks.controller !== "undefined") {
+        // Turbolinks.controller is in version 5+
+        mountEvent = "turbolinks:load";
+        unmountEvent = "turbolinks:before-cache";
+      } else {
+        mountEvent = 'page:change';
+        unmountEvent = 'page:receive';
+        Turbolinks.pagesCached(0);
+        if (window.ReactRailsUJS.RAILS_ENV_DEVELOPMENT) {
+          console.warn('The Turbolinks cache has been disabled (Turbolinks >= 2.4.0 is recommended).');
+        }
       }
     }
 
-    $(document).on('page:change', function(){
+    $(document).on(mountEvent, function(){
       riotRails.mountAll();
     });
 
     $(document).on(unmountEvent, function(){
       riotRails.unmountAll();
     });
+
   };
 
   if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {

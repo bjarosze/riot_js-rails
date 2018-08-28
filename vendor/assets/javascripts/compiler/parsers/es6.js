167dac16d03a78bcf61594c96cf6df0266f9e4db
studeba <mark@fthat.com>
2018-08-27 20:52:12 -0400

    Updating to Riot v3.11.1

--- vendor/assets/javascripts/compiler/parsers/es6.js
+++ vendor/assets/javascripts/compiler/parsers/es6.js
@@ -6,9 +6,21 @@
   -------
   2016-03-09: Initital release
 */
+
+// The babel core should be handled differently in its newest version
+// this function will allow the import of the right babel package installed
+function getBabelCore() {
+  try {
+    return require('@babel/core')
+  } catch (_) {
+    console.log('Fallback to babel-core. You might want to upgrade to @babel/core soon.')
+    return require('babel-core')
+  }
+}
+
 var
   mixobj = require('./_utils').mixobj,
-  parser = require('babel-core')
+  parser = getBabelCore()
 
 module.exports = function _babel (js, opts, url) {
 
